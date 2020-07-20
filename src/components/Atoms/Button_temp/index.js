085ec32ecc90d57a8d3bd8dbbf6a586bc3e7c7a2
Arpit Bhardwaj <42579371+arpitbhardwaj24@users.noreply.github.com>
2020-07-20 15:58:02 +0530

    Fixed Spacebreak bug

--- src/components/Atoms/Button_temp/index.js
+++ src/components/Atoms/Button_temp/index.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function button(props) {
   return (
     <div>
-          <div class="divide-red-100">hey</div>
-      <h1 classname="bg-white">hello</h1>
+      <button style={{ fontSize: 32, color: "red" }}>click</button>
+      <h1 class="bg-white">Hey there</h1>
     </div>
   );
 }
